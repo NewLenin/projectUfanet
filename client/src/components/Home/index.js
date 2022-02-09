89a1c5ad09c4e3494636694647da1c4d792332d7
SpoNNN <marisgusmanov@gmail.com>
2022-02-09 12:56:37 +0500

    ÿ

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react';
-import './style.css';
+import React, { Component } from "react";
+import "./style.css";
+import UploadAndDisplayImage from "./UploadImage/UploadImg";
+import EForm from "./EncodeForm/EForm";
 
 class Home extends Component {
   render() {
     return (
-      <h1>Home</h1>
-    )
+      <React.Fragment>
+        <div id="back">
+        <div class="container">
+          
+        
+        <div className="row" >
+          <div className="col-6 p-3">
+            <UploadAndDisplayImage />
+          </div>
+          <div className="col-6 p-3">
+            <EForm />
+          </div>
+          </div>
+          </div>
+        </div>{" "}
+      </React.Fragment>
+    );
   }
 }
 
