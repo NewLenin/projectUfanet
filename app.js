409eabac820b63d80836a860bab6b8b994177df9
SpoNNN <marisgusmanov@gmail.com>
2022-02-10 13:01:57 +0500

    õ

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
  */
 
 var express = require('express')
+  , fileupload = require("express-fileupload")
   , http = require('http')
   , path = require('path')
   , bodyParser = require('body-parser')
@@ -11,9 +12,12 @@ var express = require('express')
   , logger = require('morgan')
   , methodOverride = require('method-override');
 const { token } = require('morgan');
-const aaa =0;
+const aaa = 0;
 var app = express();
 var a = 0;
+
+app.use(fileupload());
+
 app.set('port', process.env.PORT || 3001);
 app.use(favicon(__dirname + '/public/images/favicon.png'));
 app.use(logger('dev'));
@@ -35,47 +39,58 @@ try {
   console.log(e);
 }
 const imageToBase64 = require('image-to-base64'); // запорос на модуль image-to-base64 "ИСПОЛЬЗОВАТЬ В CMD: npm i -S image-to-base64"
-const { response } = require('express');
 
-imageToBase64('imgconvert') //"имя файла"
-.then(
+imageToBase64('C:/fakepath/') //"имя файла"
+  .then(
     (response) => {
       var logger = fs.createWriteStream('log.txt', { //создание лога
-        flags: 'a' 
+        flags: 'a'
       })
       logger.write(response); // запись в log.txt
 
 
-// вывод в консоли
+      // вывод в консоли
     }
-)
-.catch(
+  )
+  .catch(
     (error) => {
-        console.log(error); 
+      console.log(error);
     }
-);
+  );
 
 
-try {  
+try {
   var data = fs.readFileSync('log.txt', 'utf8'); //читаем 
- // console.log(data.toString());    
-} catch(e) {
+  // console.log(data.toString());    
+} catch (e) {
   console.log('Error:', e.stack);
 }
-app.get('/crypt',(req,res)=>{
+app.get('/crypt', (req, res) => {
   res.json(data.toString());
-  })
-  
+})
 
- //let data = '/9j/2wCEAAMCAgMCAgMDAwMEAwMEBQgFBQQEBQoHBwYIDAoMDAsKCwsNDhIQDQ4RDgsLEBYQERMUFRUVDA8XGBYUGBIUFR QBAwQEBQQFCQUFCRQNCw0UFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFP/AABEIADgAOAMBIgACEQEDEQH/xAGiAAABBQEBAQEBAQAAAAAAAAAAAQIDBAUGBwgJCgsQAAIBAwMCBAMFBQQEAAABfQECAwAEEQUSITFBBhNRYQcicRQygZGhCCNCscEVUtHwJDNicoIJChYXGBkaJSYnKCkqNDU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6g4SFhoeIiYqSk5SVlpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2drh4uPk5ebn6Onq8fLz9PX29/j5+gEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoLEQACAQIEBAMEBwUEBAABAncAAQIDEQQFITEGEkFRB2FxEyIygQgUQpGhscEJIzNS8BVictEKFiQ04SXxFxgZGiYnKCkqNTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqCg4SFhoeIiYqSk5SVlpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2dri4+Tl5ufo6ery8/T19vf4+fr/2gAMAwEAAhEDEQA/APsa2A/s+36scV4P460K00D4h35fEcWoqt6oLA7s5WQYI/vAn6NX0v4R8OtrQcynyrOE4Lep9B71F8VvgVo3xL0S0SNv7M1TT2MlrfRKM8jlH7shwCR6gHPUGMy/eU3Tj8R1ZRalWVWp8J8w+E/i1qfhnxbF4d8uXULLCi2c7n/dsOUYnnAPAz2xXrHj34L3PxMt7d4bpNISNxLG2SZAcdMY9a8z8F6RaeH/AIkKdYdYWiJQiQZJYAjr/nrX0yniG0gsFmQhoyDtK187g5OpSfO7+R9PjIxhVTpq3mcRFo+u2dvZWOs3CXv2dRGk6ZG/HQkHvgfrW3taOEJjbgfSsbxH4yjcLJGSGVgQT0/GrVrrtjqiboZjI/dcdD6V9RhMRGa5JPVHyOOw0qcueK0YiQ+W8hJBJHr0pdv0/Kp5JEiDBUBYjkmoPtB9Fr1LHk3R6zbTRabZx2sKgQrgKPX1J9zWd40e6m8PT/ZJ/IkVc/LkMeK53U9be2lgXzFjAyXB6/y5pjeKY9S0+RI2IflSrDk+9fK1Y+0hJN6s+spNU5RaWx8U/EGz8S6v4i/tC3ma3mV9iQqA24DuzHv/AJzWv+yZr/xK1rxD438L+NIJ203TQs1lfvEAgBbBjDAYIIww5yOa9Z1jw9K+qzPKfkz+7UHAXsSf8/lXYaVew2dhZaVC32WBmHygDzJPfHbv1rwMvhOMpRktD6PMKkJxjKL1POfFutppt79jjJdu/vU3hDWYrK9NyQBI/wB5cfzrlfjzpWpvfLLorwRyA4McyF1lXnjKkbT6Hke1P8FaPqNzHb26JJc3IRQzAclscnivVpRqe0Tijzq7pew957nvGnrN4jdFgTYhxuOK1f8AhCJv+ehq14ZYaFokMNwqxXBXk5rQ/ttf+eyf99D/ABr7aCvFXPzyfxOxw3inVyLLzM7X29Pf1rkPD3iRf7SARmMx4fecALW9eW8epxPDKXAPQjqDXhnj7xRqPgXU9kFobhc5WYNjv0NeHVwrhK62Pco4xTjyy3Pf9e2uV2gFmGQce1YLTwaMTOD51wUYtJ1wMdAfzFc74Q8cP4m8KWl1NiO5K5lRW3bG7jNcp4/+JumeD7UtezAM4ISMdXODgVgqcd0jt9tLZsv+KtYfUZWkm2qEc57DgD/69Zvhr4lW+iSSCzTzbhvlVgK8M1D4ta94k1maKxs1W1B3Fmbheec+vX9Ktw+Ik0aIHIM55Z/f2rpoR5Z3scmIqKcLJn1foniUTQ/btZvhBGFOUZ/6Vf8A+E68K/8AQSj/AO+x/jXxTq3jLVddXyUmlaPPTccH/GsrGperV66qHjOld6H3tZs80wK8AL2rkvHvhE69AcW5bBwD3rr9I6n/AHf61Pe/8ex/3quSTWpzKTT0Pme0stT8E6sZ7W1kuIxlZIV4DjHqeK4b4haVa+JNVgvtSdrCCCTzMM4Lvx90AE4+te3eIP8AXzfj/SvDviT/AKj8RXmewjKdz1ViJRp8pQLS6gPsukWv2PTwOJCOoP15NX7LwfbxqHuCZXHO5u5p3hj/AJBkX+6K6GT/AFNaN20Q1rqVdN0m1kkLLGFReAfWtT+yrX+5+tVtH/49j/v1o0XYz//Z';
+
+//let data = '/9j/2wCEAAMCAgMCAgMDAwMEAwMEBQgFBQQEBQoHBwYIDAoMDAsKCwsNDhIQDQ4RDgsLEBYQERMUFRUVDA8XGBYUGBIUFR QBAwQEBQQFCQUFCRQNCw0UFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFP/AABEIADgAOAMBIgACEQEDEQH/xAGiAAABBQEBAQEBAQAAAAAAAAAAAQIDBAUGBwgJCgsQAAIBAwMCBAMFBQQEAAABfQECAwAEEQUSITFBBhNRYQcicRQygZGhCCNCscEVUtHwJDNicoIJChYXGBkaJSYnKCkqNDU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6g4SFhoeIiYqSk5SVlpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2drh4uPk5ebn6Onq8fLz9PX29/j5+gEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoLEQACAQIEBAMEBwUEBAABAncAAQIDEQQFITEGEkFRB2FxEyIygQgUQpGhscEJIzNS8BVictEKFiQ04SXxFxgZGiYnKCkqNTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqCg4SFhoeIiYqSk5SVlpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2dri4+Tl5ufo6ery8/T19vf4+fr/2gAMAwEAAhEDEQA/APsa2A/s+36scV4P460K00D4h35fEcWoqt6oLA7s5WQYI/vAn6NX0v4R8OtrQcynyrOE4Lep9B71F8VvgVo3xL0S0SNv7M1TT2MlrfRKM8jlH7shwCR6gHPUGMy/eU3Tj8R1ZRalWVWp8J8w+E/i1qfhnxbF4d8uXULLCi2c7n/dsOUYnnAPAz2xXrHj34L3PxMt7d4bpNISNxLG2SZAcdMY9a8z8F6RaeH/AIkKdYdYWiJQiQZJYAjr/nrX0yniG0gsFmQhoyDtK187g5OpSfO7+R9PjIxhVTpq3mcRFo+u2dvZWOs3CXv2dRGk6ZG/HQkHvgfrW3taOEJjbgfSsbxH4yjcLJGSGVgQT0/GrVrrtjqiboZjI/dcdD6V9RhMRGa5JPVHyOOw0qcueK0YiQ+W8hJBJHr0pdv0/Kp5JEiDBUBYjkmoPtB9Fr1LHk3R6zbTRabZx2sKgQrgKPX1J9zWd40e6m8PT/ZJ/IkVc/LkMeK53U9be2lgXzFjAyXB6/y5pjeKY9S0+RI2IflSrDk+9fK1Y+0hJN6s+spNU5RaWx8U/EGz8S6v4i/tC3ma3mV9iQqA24DuzHv/AJzWv+yZr/xK1rxD438L+NIJ203TQs1lfvEAgBbBjDAYIIww5yOa9Z1jw9K+qzPKfkz+7UHAXsSf8/lXYaVew2dhZaVC32WBmHygDzJPfHbv1rwMvhOMpRktD6PMKkJxjKL1POfFutppt79jjJdu/vU3hDWYrK9NyQBI/wB5cfzrlfjzpWpvfLLorwRyA4McyF1lXnjKkbT6Hke1P8FaPqNzHb26JJc3IRQzAclscnivVpRqe0Tijzq7pew957nvGnrN4jdFgTYhxuOK1f8AhCJv+ehq14ZYaFokMNwqxXBXk5rQ/ttf+eyf99D/ABr7aCvFXPzyfxOxw3inVyLLzM7X29Pf1rkPD3iRf7SARmMx4fecALW9eW8epxPDKXAPQjqDXhnj7xRqPgXU9kFobhc5WYNjv0NeHVwrhK62Pco4xTjyy3Pf9e2uV2gFmGQce1YLTwaMTOD51wUYtJ1wMdAfzFc74Q8cP4m8KWl1NiO5K5lRW3bG7jNcp4/+JumeD7UtezAM4ISMdXODgVgqcd0jt9tLZsv+KtYfUZWkm2qEc57DgD/69Zvhr4lW+iSSCzTzbhvlVgK8M1D4ta94k1maKxs1W1B3Fmbheec+vX9Ktw+Ik0aIHIM55Z/f2rpoR5Z3scmIqKcLJn1foniUTQ/btZvhBGFOUZ/6Vf8A+E68K/8AQSj/AO+x/jXxTq3jLVddXyUmlaPPTccH/GsrGperV66qHjOld6H3tZs80wK8AL2rkvHvhE69AcW5bBwD3rr9I6n/AHf61Pe/8ex/3quSTWpzKTT0Pme0stT8E6sZ7W1kuIxlZIV4DjHqeK4b4haVa+JNVgvtSdrCCCTzMM4Lvx90AE4+te3eIP8AXzfj/SvDviT/AKj8RXmewjKdz1ViJRp8pQLS6gPsukWv2PTwOJCOoP15NX7LwfbxqHuCZXHO5u5p3hj/AJBkX+6K6GT/AFNaN20Q1rqVdN0m1kkLLGFReAfWtT+yrX+5+tVtH/49j/v1o0XYz//Z';
 //сам текст в base64
- let buff = new Buffer(data, 'base64');// читаем, выставлем кодировку  
- fs.writeFileSync('image.png', buff);//создание и запись
+let buff = new Buffer(data, 'base64');// читаем, выставлем кодировку  
+fs.writeFileSync('image.png', buff);//создание и запись
 
- console.log('success');
- app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.post('/sendImg', (req, res) => {
+  var file;
+
+  if (!req.files) {
+    console.log('not ok');
+    // res.send("File was not found");
+    return;
+  }
+
+  file = req.files.imgForm;  // here is the field name of the form
+  console.log('ok');
+  // res.send("File Uploaded");
 });
-http.createServer(app).listen(app.get('port'), function(){
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+http.createServer(app).listen(app.get('port'), function () {
   console.log("Express server listening on port " + app.get('port'));
 });
--- client/src/components/Home/UploadImage/UploadImg.js
+++ client/src/components/Home/UploadImage/UploadImg.js
@@ -4,6 +4,25 @@ import "./UploadStyle.css";
 const UploadAndDisplayImage = () => {
   const [selectedImage, setSelectedImage] = useState(null);
 
+  function send(e){
+    e.preventDefault();
+    var file = document.forms.imgForm;
+    // var file = document.forms.imgForm.elements.myImage.value;
+    // var file = document.forms.imgForm.elements.myImage.value.get[0].files[0];
+    //
+    //
+    //         var newFileName = file.filename + "new";
+    //         var formData = new FormData();
+    //         formData.append('file', file, newFileName);
+    // console.log( document.forms.form.elements.myImage.value);
+    
+    let XHR = new XMLHttpRequest();
+    XHR.open("POST", "/sendImg");
+    XHR.send(file);
+
+    console.log(file);
+  }
+
   return (
     <React.Fragment>
       {selectedImage && (
@@ -13,7 +32,6 @@ const UploadAndDisplayImage = () => {
             alt="not found"
             src={URL.createObjectURL(selectedImage)}
           />
-          <br />
           <button
             className="btn btn-danger Del"
             onClick={() => setSelectedImage()}
@@ -22,16 +40,19 @@ const UploadAndDisplayImage = () => {
           </button>
         </div>
       )}
-      <br />
-      <input
-        type="file"
-        name="myImage"
-        className="form-control forma"
-        onChange={(event) => {
-          console.log(event.target.files);
-          setSelectedImage(event.target.files[0]);
-        }}
-      />
+      <form enctype="multipart/form-data" method="POST" action="" onSubmit={(event)=>send(event)} name="imgForm">
+        <input
+          type="file"
+          name="myImage"
+          className="form-control forma"
+          onChange={(event) => {
+            console.log(event.target.files);
+            setSelectedImage(event.target.files[0]);
+          }}
+        />
+        <button type="submit">Send</button>
+      </form>
+
     </React.Fragment>
   );
 };
