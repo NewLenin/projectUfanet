ef6274f37c699129431511991f1597216cc47261
SpoNNN <marisgusmanov@gmail.com>
2022-02-17 12:36:03 +0500

    MAris zakonchil

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -10,7 +10,15 @@ class Home extends Component {
     super();
     this.state = {
       files: [],
+      text: "",
     };
+  }
+  handleChange(event) {
+
+    let value = event.target.value;
+    this.setState({ text: value });
+    console.log(this.state.text);
+
   }
 
   getFiles(files) {
@@ -31,10 +39,25 @@ class Home extends Component {
         </button>
         <label className={"Text3"}>Image to Base64</label>
         <div className={"Line"}></div>
-<<<<<<< HEAD
-=======
         <button className={"But1"}><label className={"Text6"}>Downlowd</label></button>
->>>>>>> e2c936db136fdf3ca462d22210c0502c71a389dd
+        <div>
+          <form className={"Box1"}>
+
+            <img
+                
+              src={"data:image/jpeg;base64," + this.state.text}
+            />
+
+
+          </form>
+        </div>
+        <textarea
+          className={"Encode64"}
+          type="text"
+          onChange={this.handleChange.bind(this)}
+          id="baseStr64text"
+          name="baseStr64text"
+        ></textarea>
         <div className={"Backf"}>
           <div>
             <div>
@@ -43,88 +66,34 @@ class Home extends Component {
               </div>
               <div className="Box">
                 {this.state.files.map((file, i) => {
-                  return <img key={i} src={file.base64} />;
+                  return <img key={i} src={this.state.files[0].base64} />;
                 })}
               </div>
             </div>
 
-            <input
-<<<<<<< HEAD
-              className={"Encode64"}
-              type="text"
-
-=======
-            className={"Encode64"}
-              type="text"
-              
->>>>>>> e2c936db136fdf3ca462d22210c0502c71a389dd
-              id="baseStr64text"
-              name="baseStr64text"
-            ></input>
-
-<<<<<<< HEAD
 
 
 
-            <div>
-              <form>
-=======
-            <div>
-              <form className={"Box1"}>
->>>>>>> e2c936db136fdf3ca462d22210c0502c71a389dd
-                {this.state.files.map((file, i) => {
-                  let baseStr64 = document.getElementById("baseStr64text")
-                    .value;
-                  return (
-                    <img
-<<<<<<< HEAD
-                      hidden
-=======
-                      
->>>>>>> e2c936db136fdf3ca462d22210c0502c71a389dd
-                      key={i}
-                      src={"data:image/jpg;base64," + baseStr64}
-                    />
-                  ); //реализуйте вывод пж из base64 в картинку
-                })}
-              </form>
-            </div>
             <label className={"Text5"}>Base64 to Image</label>
-<<<<<<< HEAD
-        
-
-            {this.state.files.length != 0 ? (
-              
-              <div><button className={"Copy"} type="submit">
-              <label className={"Text4"}>Copy</label>
-            </button>
-                <div
-=======
             <button className={"Copy"} type="submit">
               <label className={"Text4"}>Copy</label>
             </button>
             {this.state.files.length != 0 ? (
               <div>
-                <textarea
->>>>>>> e2c936db136fdf3ca462d22210c0502c71a389dd
+                <div
                   className={"Code64"}
                   placeholder="Leave a comment here"
                   id="floatingTextarea2"
                 >
-<<<<<<< HEAD
-                      
-                  {JSON.stringify(this.state.files[0].base64)}
+                  {JSON.stringify(this.state.files[0].base64).slice(1,-1)}
                 </div>
-=======
-                  {JSON.stringify(this.state.files[0].base64)}
-                </textarea>
->>>>>>> e2c936db136fdf3ca462d22210c0502c71a389dd
               </div>
             ) : null}
 
           </div>
-        </div>{" "}
-      </React.Fragment>
+        </div>
+
+      </React.Fragment >
     );
   }
 }
